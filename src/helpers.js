aa4ae53f635c5768b1358abaa8eacc4f8855821b
pgil256 <113308245+pgil256@users.noreply.github.com>
2023-03-29 16:54:59 -0400

    Add files via upload

---
+++ src/helpers.js
@@ -0,0 +1,19 @@
+import uuid from 'uuid';
+
+/* Select a random element from values array. */
+function choice(values) {
+  const randIdx = Math.floor(Math.random() * values.length);
+  return values[randIdx];
+}
+
+/* Format response data from the Deck of Cards API,
+ * extracting just the image url. */
+function formatCard(data) {
+  return {
+    image: data.cards[0].image,
+    id: uuid()
+  };
+}
+
+
+export { choice, formatCard };
